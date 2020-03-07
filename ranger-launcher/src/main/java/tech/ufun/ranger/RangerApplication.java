d841d2d739bbaf91b1439b3ef1dbde6c3ab2cf0e
张雷 <zhanglei@yundait.com>
2020-03-07 11:51:02 +0800

    添加maven多配置文件

--- ranger-launcher/src/main/java/tech/ufun/ranger/RangerApplication.java
+++ ranger-launcher/src/main/java/tech/ufun/ranger/RangerApplication.java
@@ -2,6 +2,11 @@ package tech.ufun.ranger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Springboot启动类
@@ -11,7 +16,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class RangerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(RangerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(RangerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+
+        System.out.println("启动成功");
+        System.out.println("访问地址：http://localhost:" + port + path);
+        System.out.println("访问地址：http://" + ip + ":" + port + path);
     }
 }
